806e3e7f2f211c3721597632c42371907f6a0fbe
Pedro Henrique Conrado <conradohpedro@gmail.com>
2021-10-22 14:20:26 -0300

    first commit

---
+++ web/src/components/MessageList/index.tsx
@@ -0,0 +1,50 @@
+import { api } from '../../services/api';
+
+import styles from './styles.module.scss'
+
+import logoImg from '../../assets/logo.svg'
+import { useEffect } from 'react';
+
+export function MessageList() {
+    useEffect(() => {
+        api.get('messages/last3').then(response => {
+            console.log(response.data)
+        })
+    }, []);
+
+    return (
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="DoWhile 2021" />
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/pedrohconrado.png" alt="Pedro Conrado" />
+                        </div>
+                        <span>Pedro Conrado</span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/pedrohconrado.png" alt="Pedro Conrado" />
+                        </div>
+                        <span>Pedro Conrado</span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/pedrohconrado.png" alt="Pedro Conrado" />
+                        </div>
+                        <span>Pedro Conrado</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
+}
